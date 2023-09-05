242724d89b39826fc8c7773c02a450d9f13ba6a1
Lungu J David <lungudjos@gmail.com>
2023-09-05 17:37:18 +0200

    the story of the body

---
+++ .history/Program_20230905170654.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// learning more about writing to the console in a console application
+Console.WriteLine("Short Life Story of a Little Boy");
+Console.Write("This male child was born in T.Ndola on the early hours of one Saturday morning.");
+Console.WriteLine(" The father and the mother were all resident of the same city, they were of the Christian faith.");
+Console.WriteLine("At the time when the boy was born the father and the mother were not employed in any company. Life was so difficult during this era of the parents.");
+Console.WriteLine("It seemed as though there would not be any light at the end of the turnel, ");
+
---
+++ .history/Program_20230905171857.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// learning more about writing to the console in a console application
+Console.WriteLine("Short Life Story of a Little Boy");
+Console.Write("There was a first born male child that was born in a certain city on the early hours of one Saturday morning.");
+Console.WriteLine(" The father and the mother were all resident of the same city, they were of the Christian faith.");
+Console.WriteLine("At the time when the boy was born the father and the mother were not employed in any company. Life was so difficult during this era of the parents.");
+Console.WriteLine("It seemed as though there would not be any light at the end of the tunnel, but the God they believed in made a way for them and at the time they were having the fourth doors opened.");
+Console.WriteLine("Years went by until the family was blessed with seven kids, three boys and four girls, the boys were the first, second, second-last born child and the rest were girls.");
+Console.WriteLine("The lad became of age and all his life time he was also devoted to the faith of his parents. He hoped everything would flourish and go on as the usual");
